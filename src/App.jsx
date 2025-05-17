259a747e5e8811ca0a29d9362c23df1fb87ab026
yungflashh <streaktv8@gmail.com>
2025-05-17 23:26:38 +0100

    added a lil update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import HomePage from "./pages/Homepage/HomePage"
 import Footer from "./static/Header/Footer"
 import Header from "./static/Header/Header"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import About from "./pages/AboutPage/About"
 import { myValue } from "./context/NavState";
 
 
